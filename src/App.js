cad8e5244d85b697b5da983aae76e5b11b5a3f45
Yeelang <yeelangy105@gmail.com>
2023-03-11 16:04:44 +0700

    First commit

---
+++ src/App.js
@@ -0,0 +1,82 @@
+import React, { useState } from 'react';
+import './App.css';
+import { Box, Button, Container, TextField, Typography } from '@mui/material';
+function App() {
+  var [value, setValue] = useState('');
+  let [output, setOutput] = useState(0);
+  let restrvalue = value.replace(/\s/g, '');
+  function Cacul() {
+    for (let i = 0; i < restrvalue.length; i++) {
+      let newVlaue = restrvalue[i];
+      if (newVlaue === '+') {
+        let rnum = parseFloat(restrvalue.slice(0, i));
+        let lnum = parseFloat(restrvalue.slice(i + 1, restrvalue.length));
+        setOutput(rnum + lnum);
+      } else if (newVlaue === '-') {
+        let rnum = parseFloat(restrvalue.slice(0, i));
+        let lnum = parseFloat(restrvalue.slice(i + 1, restrvalue.length));
+        setOutput(rnum - lnum);
+      } else if (newVlaue === 'x') {
+        let rnum = parseFloat(restrvalue.slice(0, i));
+        let lnum = parseFloat(restrvalue.slice(i + 1, restrvalue.length));
+        setOutput(rnum * lnum);
+      } else if (newVlaue === '/') {
+        let rnum = parseFloat(restrvalue.slice(0, i));
+        let lnum = parseFloat(restrvalue.slice(i + 1, restrvalue.length));
+        setOutput(rnum / lnum);
+      } else {
+        console.log('Invalid operand or wrong input');
+      }
+    }
+  }
+
+  return (
+    <div className="App">
+      <Box sx={{ mt: '6vh' }}>
+        <Container
+          maxWidth="xs"
+          sx={{
+            mt: '2vh',
+            boxShadow: '0px 0.5px 1px 1px #c6c6c6',
+            borderRadius: '10px',
+          }}
+        >
+          <Box
+            sx={{
+              display: 'flex',
+              flexDirection: 'column',
+              justifyContent: 'center',
+            }}
+          >
+            <Typography variant="h5" sx={{ mt: 2, mb: 3 }}>
+              Calculate
+            </Typography>
+            <TextField
+              id="outlined-basic"
+              label="Input:"
+              variant="outlined"
+              sx={{ mb: 2 }}
+              size="small"
+              value={value}
+              onChange={(e) => setValue(e.target.value)}
+            />
+
+            <Typography variant="h5" sx={{ mt: 2, mb: 3 }}>
+              Output:{` ${output}`}
+            </Typography>
+            <Button
+              variant="contained"
+              onClick={Cacul}
+              color="success"
+              sx={{ mb: 3, mt: 3 }}
+            >
+              Execute
+            </Button>
+          </Box>
+        </Container>
+      </Box>
+    </div>
+  );
+}
+
+export default App;
